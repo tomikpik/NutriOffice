cbcea80e4584bb949d7a6d97b47058ced7b1f6ee
pikous <tomas.pikous@gmail.com>
2015-05-15 20:37:30 +0200

    as

--- src/main/java/gui/layout/excerciseDialog/ExcerciseDialogController.java
+++
@@ -1,7 +0,0 @@
-package gui.layout.excerciseDialog;
-
-/**
- * Created by Tom on 14. 5. 2015.
- */
-public class ExcerciseDialogController {
-}
--- src/main/java/gui/layout/trainingDialog/TrainingDialogController.java
+++
@@ -1,7 +0,0 @@
-package gui.layout.trainingDialog;
-
-/**
- * Created by Tom on 14. 5. 2015.
- */
-public class TrainingDialogController {
-}
